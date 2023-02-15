779e8c9cf464e99d36337ac293a479f55a998d13
Sebasreboreda <119048767+Sebasreboreda@users.noreply.github.com>
2023-02-15 19:22:43 +0100

    Update Jugadores.java

--- Ruleta/src/ruleta/Jugadores.java
+++ Ruleta/src/ruleta/Jugadores.java
@@ -9,5 +9,37 @@ package ruleta;
  * @author sebas
  */
 class Jugadores {
-    
+    private int numero;
+    private int pactual;
+    private int ptotal;
+
+    public Jugadores(int numero, int pactual, int ptotal) {
+        this.numero = numero;
+        this.pactual = pactual;
+        this.ptotal = ptotal;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public int getPactual() {
+        return pactual;
+    }
+
+    public void setPactual(int pactual) {
+        this.pactual = pactual;
+    }
+
+    public int getPtotal() {
+        return ptotal;
+    }
+
+    public void setPtotal(int ptotal) {
+        this.ptotal = ptotal;
+    }
 }
